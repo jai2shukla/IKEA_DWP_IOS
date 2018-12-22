f0f53eca0298fd5a3011361506708bd82198f65e
jai2shukla <shatrunjay.s@HCL.COM>
2018-12-22 22:46:10 +0530

    Consume API Changes

--- IKEA_DWP_IOS/About.aspx.cs
+++ IKEA_DWP_IOS/About.aspx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,6 +14,37 @@ namespace IKEA_DWP_IOS
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            IEnumerable<EmployeeViewModel> Employee = null;
+
+            var client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://localhost:8081/api/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // get something
+            var responseTask = client.GetAsync("GetEmployees");
+
+            responseTask.Wait();
+
+            //To store result of web api response.   
+            var result = responseTask.Result;
+
+            //If success received   
+            if (result.IsSuccessStatusCode)
+            {
+                var data = result.Content.ReadAsStreamAsync();
+                //var readTask = result.Content.ReadAsAsync<IList<EmployeeViewModel>>();
+                data.Wait();
+                
+                //Employee = data.Result;
+            }
+            else
+            {
+                //Error response received   
+                Employee = Enumerable.Empty<EmployeeViewModel>();
+                ModelState.AddModelError(string.Empty, "Server error try after some time.");
+            }
 
         }
     }
---
+++ IKEA_DWP_IOS/EmployeeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IKEA_DWP_IOS
+{
+    public class EmployeeViewModel
+    {
+        public int ID { get; set; }  
+        public string Name { get; set; }  
+        public string Designation { get; set; }
+        public string Location { get; set; }
+
+    }
+}
