0a1a55b47784130e3f8fafe177963f1775668c2e
jai2shukla <shatrunjay.s@HCL.COM>
2018-12-30 17:27:21 +0530

    fg

--- IKEA_DWP_IOS/About.aspx.cs
+++ IKEA_DWP_IOS/About.aspx.cs
@@ -20,6 +20,7 @@ namespace IKEA_DWP_IOS
             if (!Page.IsPostBack)
             {
                 string dd = "";
+                string bb = "";
                 var data1 = 888;
                 var data = findAll();
                 grdData.DataSource = data;
